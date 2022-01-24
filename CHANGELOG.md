59d8e37bb403a1f17bcb0ee3a4bc1d32b0a81198
Dave Longley <dlongley@digitalbazaar.com>
2022-01-24 12:25:52 -0500

    Add initial files.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,4 @@
-# bedrock-module-template-http ChangeLog
+# bedrock-web-pouch-edv ChangeLog
 
 ## 1.0.0 - TBD
 
--- test/mocha/10-api.js
+++
@@ -1,7 +0,0 @@
-/*
- * Copyright (c) 2021 Digital Bazaar, Inc. All rights reserved.
- */
-
-describe('api', () => {
-  it('should work');
-});
