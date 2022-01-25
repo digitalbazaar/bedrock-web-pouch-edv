ea09abbe12f19d41dc616a4b0e5e197b642349c4
Dave Longley <dlongley@digitalbazaar.com>
2022-01-25 17:48:55 -0500

    Add initial tests.

--- test/test.config.js
+++ test/test.config.js
@@ -6,20 +6,10 @@
 const {config} = require('bedrock');
 const path = require('path');
 
-// MongoDB
-config.mongodb.name = 'bedrock_web_pouch_edv_test';
-// drop all collections on initialization
-config.mongodb.dropCollections = {};
-config.mongodb.dropCollections.onInit = true;
-config.mongodb.dropCollections.collections = [];
-
-config.mocha.tests.push(path.join(__dirname, 'mocha'));
-
 // allow self-signed certs in test framework
 config['https-agent'].rejectUnauthorized = false;
 
 config.karma.suites['bedrock-web-pouch-edv'] = path.join('web', '**', '*.js');
-config.karma.config.proxies = {
-  '/': 'https://localhost:18443'
-};
-config.karma.config.proxyValidateSSL = false;
+
+// FIXME: remove me; disables babel preprocessing
+config.karma.defaults.DEFAULT_PREPROCESSORS = ['webpack'];
--- test/web/10-api.js
+++
@@ -1,9 +0,0 @@
-/*!
- * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
- */
-describe('FIXME API', function() {
-  describe('FIXME API', function() {
-    it('should test something', async () => {
-    });
-  });
-});
