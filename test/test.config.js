551ef13ffcc2dae4e4b1c3ac08b0f494d282b05c
Dave Longley <dlongley@digitalbazaar.com>
2022-01-31 18:52:04 -0500

    Remove unused test dependencies.

--- test/test.config.js
+++ test/test.config.js
@@ -6,10 +6,8 @@
 const {config} = require('bedrock');
 const path = require('path');
 
-// allow self-signed certs in test framework
-config['https-agent'].rejectUnauthorized = false;
-
 config.karma.suites['bedrock-web-pouch-edv'] = path.join('web', '**', '*.js');
 
-// FIXME: remove me; disables babel preprocessing
+// disable babel preprocessing during karma tests to avoid
+// errors with dated bedrock-karma and features like spread operator
 config.karma.defaults.DEFAULT_PREPROCESSORS = ['webpack'];
