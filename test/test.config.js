257dec5462cae1937648d92856f496ba1ded8a58
Dave Longley <dlongley@digitalbazaar.com>
2022-05-30 17:31:58 -0400

    Update test suite framework.

--- test/test.config.js
+++ test/test.config.js
@@ -2,10 +2,17 @@
  * Copyright (c) 2022 Digital Bazaar, Inc. All rights reserved.
  */
 import {config} from '@bedrock/core';
-import path from 'path';
+import {createRequire} from 'node:module';
+import path from 'node:path';
+const require = createRequire(import.meta.url);
 
 config.karma.suites['bedrock-web-pouch-edv'] = path.join('web', '**', '*.js');
 
 // disable babel preprocessing during karma tests to avoid
 // errors with dated bedrock-karma and features like spread operator
 config.karma.defaults.DEFAULT_PREPROCESSORS = ['webpack'];
+config.karma.config.webpack.resolve = {
+  fallback: {
+    events: require.resolve('events/')
+  }
+};
